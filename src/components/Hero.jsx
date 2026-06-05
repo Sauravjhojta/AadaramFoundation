9d44f9b2855045afb3b6b30ec53d2a9f1a910c6d
Saurav Jhojta <jhojtasaurav@gmail.com>
2026-06-05 10:05:20 +0530

    hero changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,64 @@ function Hero() {
       >
         {/* Slide 1 */}
         <SwiperSlide>
-         <div className="relative h-screen">
+         
+
+          <div className="relative h-screen">
+
+            <img
+              src={plantation1}
+              alt="Plantation Drive"
+              className="w-full h-full object-cover"
+            />
+
+         <div className="absolute inset-0 bg-black/35">
+  <div className="absolute bottom-6 right-4 md:bottom-4 md:right-4 lg:bottom-24 lg:right-28 text-right max-w-3xl">
+                <h2
+                  className="
+                  text-white
+                  font-black
+                  uppercase
+                  leading-none
+                  text-4xl
+                  sm:text-5xl
+                  md:text-6xl
+                  lg:text-7xl
+                "
+                >
+                  Building A Greener
+                </h2>
+
+                <h2
+                  className="
+                  text-[#7ED957]
+                  font-black
+                  uppercase
+                  leading-none
+                  mt-2
+                  text-4xl
+                  sm:text-5xl
+                  md:text-6xl
+                  lg:text-7xl
+                "
+                >
+                  Future For All
+                </h2>
+
+               <p className="mt-6 text-white/90 text-base md:text-xl max-w-2xl ml-auto">
+  Join us in planting trees and restoring nature
+  for a cleaner, healthier and sustainable future.
+</p>
+
+              </div>
+            </div>
+
+          </div>
+          
+        </SwiperSlide>
+
+        {/* Slide 2 */}
+        <SwiperSlide>
+           <div className="relative h-screen">
 
             <img
               src={plantation2}
@@ -70,61 +127,6 @@ function Hero() {
             </div>
 
           </div>
-        </SwiperSlide>
-
-        {/* Slide 2 */}
-        <SwiperSlide>
-           <div className="relative h-screen">
-
-            <img
-              src={plantation1}
-              alt="Plantation Drive"
-              className="w-full h-full object-cover"
-            />
-
-         <div className="absolute inset-0 bg-black/35">
-  <div className="absolute bottom-4 right-4 md:bottom-4 md:right-4 lg:bottom-24 lg:right-28 text-right max-w-3xl">
-                <h2
-                  className="
-                  text-white
-                  font-black
-                  uppercase
-                  leading-none
-                  text-4xl
-                  sm:text-5xl
-                  md:text-6xl
-                  lg:text-7xl
-                "
-                >
-                  Building A Greener
-                </h2>
-
-                <h2
-                  className="
-                  text-[#7ED957]
-                  font-black
-                  uppercase
-                  leading-none
-                  mt-2
-                  text-4xl
-                  sm:text-5xl
-                  md:text-6xl
-                  lg:text-7xl
-                "
-                >
-                  Future For All
-                </h2>
-
-               <p className="mt-6 text-white/90 text-base md:text-xl max-w-2xl ml-auto">
-  Join us in planting trees and restoring nature
-  for a cleaner, healthier and sustainable future.
-</p>
-
-              </div>
-            </div>
-
-          </div>
-          
         </SwiperSlide>
 
         {/* Slide 3 */}
